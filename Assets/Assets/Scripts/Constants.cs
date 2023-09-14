6932905ba88ff697b6dd89f5e4908cc699449f87
Lost_Shadow <69916626+Ra1mun@users.noreply.github.com>
2023-09-14 22:57:26 +0700

    ase

--- Assets/Assets/Scripts/Constants.cs
+++ Assets/Assets/Scripts/Constants.cs
@@ -1,5 +1,4 @@
 public class Constants
 {
     public const float ATTTACK_INTERVAL = 0.5f;
-    public const float PERIODICAL_DAMAGE_INTERVAL = 1f;
 }
--- Assets/Assets/Scripts/Item/Projectiles/ProjectileLinear.cs
+++ Assets/Assets/Scripts/Item/Projectiles/ProjectileLinear.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileLinearSpear : Projectile
+public class ProjectileLinear : Projectile
 {
     protected override void ManualDispose()
     {
